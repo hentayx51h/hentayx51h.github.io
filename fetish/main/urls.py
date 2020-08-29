07da5327911a442def42fabf0df7ff4c350adcf4
hentayx51h <61069585+hentayx51h@users.noreply.github.com>
2020-08-30 00:05:30 +0300

    Delete urls.py

--- fetish/main/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('about', views.about, name='about'),
-    path('pricing', views.pricing, name='pricing'),
-    path('sign-in', views.signin, name='signin'),
-]
